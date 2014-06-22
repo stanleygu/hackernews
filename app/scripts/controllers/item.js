6ef90df53bd4883f1ada3c39994922ab11d35db8
Stanley Gu <stanleygu@gmail.com>
2014-06-21 22:38:38 -0700

    Added localStorageService

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -8,12 +8,11 @@
  * Controller of the angularHnApp
  */
 angular.module('angularHnApp')
-  .controller('ItemCtrl', function($scope, $routeParams, $http, placeHolder) {
-
-    $http.get('http://node-hnapi.herokuapp.com/item/' + $routeParams.id).success(function(data) {
-      $scope.item = data;
-    }).
-    error(function() {
-      $scope.item = placeHolder.item;
-    });
+  .controller('ItemCtrl', function($scope, $routeParams, $http) {
+    $scope.item = $scope.data.items[$routeParams.id];
+    if (!$scope.item) {
+      $http.get('http://node-hnapi.herokuapp.com/item/' + $routeParams.id).success(function(data) {
+        $scope.item = data;
+      });
+    }
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,35 @@
  * Controller of the angularHnApp
  */
 angular.module('angularHnApp')
-  .controller('MainCtrl', function ($scope, $http, placeHolder) {
+  .controller('MainCtrl', function ($scope, $http, placeHolder, localStorageService, $q, $log, $rootScope) {
     $http.get('http://node-hnapi.herokuapp.com/news').success(function(data) {
-      $scope.data = data;
+      $scope.data = {
+        frontPage: data,
+        time: (new Date()).getTime(),
+        items: {}
+      };
+
+      var fetchCommentsPromises = [];
+      angular.forEach($scope.data.frontPage, function(item) {
+        if(item.id) {
+          var promise = $http.get('http://node-hnapi.herokuapp.com/item/' + item.id).success(function(data) {
+            $scope.data.items[item.id] = data;
+          });
+          fetchCommentsPromises.push(promise);
+        }
+      });
+      $q.all(fetchCommentsPromises).then(function() {
+        localStorageService.set('data', $scope.data);
+        $rootScope.data = $scope.data;
+        $log.info('Saved to local storage', $scope.data);
+      });
     })
     .error(function() {
-      $scope.data = placeHolder.frontPage;
+      var data = localStorageService.get('data');
+      if (data) {
+        $scope.data = data.frontPage;
+      } else {
+        $scope.data = placeHolder.frontPage;
+      }
     });
   });
