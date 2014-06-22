f50d10d7254dc86c9aecb8afdae3007e95ce379f
Stanley Gu <stanleygu@gmail.com>
2014-06-21 23:06:28 -0700

    Remove

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,8 +137,7 @@ module.exports = function (grunt) {
           dot: true,
           src: [
             '.tmp',
-            '<%= yeoman.dist %>/{,*/}*',
-            '!<%= yeoman.dist %>/.git*'
+            '<%= yeoman.dist %>/{,*/}*'
           ]
         }]
       },
