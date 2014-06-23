e8eb6ee571cad4fc3fd57f3c18f438b58259a26e
Stanley Gu <stanleygu@gmail.com>
2014-06-23 08:59:42 -0700

    Using hacker news service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,35 +8,7 @@
  * Controller of the angularHnApp
  */
 angular.module('angularHnApp')
-  .controller('MainCtrl', function ($scope, $http, placeHolder, localStorageService, $q, $log, $rootScope) {
-    $http.get('http://node-hnapi.herokuapp.com/news').success(function(data) {
-      $scope.data = {
-        frontPage: data,
-        time: (new Date()).getTime(),
-        items: {}
-      };
-
-      var fetchCommentsPromises = [];
-      angular.forEach($scope.data.frontPage, function(item) {
-        if(item.id) {
-          var promise = $http.get('http://node-hnapi.herokuapp.com/item/' + item.id).success(function(data) {
-            $scope.data.items[item.id] = data;
-          });
-          fetchCommentsPromises.push(promise);
-        }
-      });
-      $q.all(fetchCommentsPromises).then(function() {
-        localStorageService.set('data', $scope.data);
-        $rootScope.data = $scope.data;
-        $log.info('Saved to local storage', $scope.data);
-      });
-    })
-    .error(function() {
-      var data = localStorageService.get('data');
-      if (data) {
-        $scope.data = data.frontPage;
-      } else {
-        $scope.data = placeHolder.frontPage;
-      }
-    });
+  .controller('MainCtrl', function ($rootScope, hackerNewsData) {
+    hackerNewsData.getData();
+    $rootScope.refresh = hackerNewsData.getData;
   });
