a73e0ace5e03fd47eaeaee02cffbe93e82c01845
Stanley Gu <stanleygu@gmail.com>
2014-06-23 10:02:30 -0700

    Added scroll saving and visit history

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -8,7 +8,8 @@
  * Controller of the angularHnApp
  */
 angular.module('angularHnApp')
-  .controller('ItemCtrl', function($scope, $routeParams, $http) {
+  .controller('ItemCtrl', function($scope, $routeParams, $http, $window) {
+    $window.scrollTo(0, 0);
     $scope.item = $scope.data.items[$routeParams.id];
     if (!$scope.item) {
       $http.get('http://node-hnapi.herokuapp.com/item/' + $routeParams.id).success(function(data) {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,32 @@
  * Controller of the angularHnApp
  */
 angular.module('angularHnApp')
-  .controller('MainCtrl', function ($rootScope, hackerNewsData) {
+  .controller('MainCtrl', function(
+    $scope,
+    $rootScope,
+    hackerNewsData,
+    $cacheFactory,
+    $anchorScroll,
+    $location,
+    $window,
+    localStorageService
+  ) {
     hackerNewsData.getData();
     $rootScope.refresh = hackerNewsData.getData;
+
+    $scope.history = localStorageService.get('history') || {};
+
+    var cache = $cacheFactory.get('scroll') || $cacheFactory('scroll');
+
+    var previousItem = $location.hash(cache.get('item'));
+    if (previousItem) {
+      $anchorScroll();
+      $window.scrollBy(0, -60);
+    }
+
+    $scope.setScrollItem = function(id) {
+      cache.put('item', id);
+      $scope.history[id] = true;
+      localStorageService.set('history', $scope.history);
+    };
   });
